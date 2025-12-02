1a3a1ae07ff0d7ebc637bdaf8c628fe8e7532167
Daniel Na <element11sodium@gmail.com>
2025-12-01 21:43:26 -0500

    1p1

---
+++ 1/main.c
@@ -0,0 +1,28 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main() {
+    FILE* fd = fopen("./input.txt", "r");
+    if (fd == NULL) return 1;
+
+    size_t bytes;
+    char dir;
+    int dist, total = 0, cur = 50;
+    while (fscanf(fd, "%c%d\n", &dir, &dist) == 2) {
+        if (dir == 'L') {
+            if (dist > cur) 
+                while (dist > cur) cur += 100;
+            cur -= dist;
+        } else if (dir == 'R') {
+            cur += dist;
+            if (cur > 99)
+                while (cur > 99) cur -= 100;
+        } else {
+            printf("FAILED, got %c, %d\n",dir, dist);
+            break;
+        }
+        if (cur == 0) total++;
+    }
+    printf("The password is %d\n", total); 
+    return 0;
+}
