be9ac1aaefb5420760ec3366d89267b684a5b448
Daniel Na <element11sodium@gmail.com>
2025-12-01 22:10:55 -0500

    failing on part 2

--- 1/main.c
+++ 1/main.c
@@ -9,19 +9,26 @@ int main() {
     char dir;
     int dist, total = 0, cur = 50;
     while (fscanf(fd, "%c%d\n", &dir, &dist) == 2) {
+        if (dist == 0) continue;
         if (dir == 'L') {
-            if (dist > cur) 
-                while (dist > cur) cur += 100;
+            if (dist > cur) { 
+                while (dist > cur) {
+                    total++;
+                    cur += 100;
+                }
+            }
             cur -= dist;
         } else if (dir == 'R') {
             cur += dist;
-            if (cur > 99)
-                while (cur > 99) cur -= 100;
+            if (cur > 99) {
+                while (cur > 99) {
+                    total++;
+                    cur -= 100;
+                }
+            }
         } else {
-            printf("FAILED, got %c, %d\n",dir, dist);
             break;
         }
-        if (cur == 0) total++;
     }
     printf("The password is %d\n", total); 
     return 0;
